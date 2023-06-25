6a5a59157829554b1ee6f1f7d0b957947d7d8d6d
Duong Nguyen <duong.nguyen@esgtech.co>
2023-06-25 11:08:13 +0700

    Modelling Users

--- models/userModel.js
+++ models/userModel.js
@@ -1,26 +1,39 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   address: {
     type: String,
+    required: [true, 'Please provide your address'],
   },
   email: {
     type: String,
+    required: [true, 'Please provide your email!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
   id: {
     type: String,
   },
   name: {
     type: String,
+    required: [true, 'Please tell us your name!'],
   },
   password: {
     type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
   },
   phone: {
     type: String,
+    required: [true, 'Please provide your phone number'],
+    validate: [validator.isMobilePhone, 'Please provide a valid phone number']
   },
   username: {
     type: String,
+    required: [true, 'Please provide your username!'],
+    unique: true,
   },
 }); 
 
