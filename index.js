b7019c550938161588687ed61d47ddbc6d4b90db
Duong Nguyen <duong.nguyen@esgtech.co>
2023-06-25 13:46:59 +0700

    Errors Outside Express: Unhandled Rejections

--- index.js
+++ index.js
@@ -25,3 +25,11 @@ const port = process.env.PORT || 3001;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHADLED REJECTION! 💥 Shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
+});
